282c63024a321128b19f2bc88c5eb5f707f6ddf7
Citlaliitzel <chica3334@gmail.com>
2023-10-30 23:48:59 -0500

    aact

--- Practica 2/.venv/Scripts/activate.bat
+++ Practica 2/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\perez\OneDrive\Escritorio\9no Semestre\python-2\Practica 2\.venv
+set VIRTUAL_ENV=c:\practicas\python-2\Practica 2\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
