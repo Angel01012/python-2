088d67b3f74d4ea47598242c8e839c50ab4331db
MyWolfc <juanitoamerica1@hotmail.com>
2023-11-21 23:27:35 -0600

    act fomras

--- Proyecto Servicios/PCS/forms.py
+++ Proyecto Servicios/PCS/forms.py
@@ -1,14 +1,38 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
-from wtforms .validators import DataRequired
+from wtforms.validators import DataRequired
 
-class ProductosForm(FlaskForm):
-    nombre = StringField('Nombre',validators=[DataRequired()])
-    descripcion = StringField('descripcion')
-    precio = StringField('precio',validators=[DataRequired()])
-    enviar = SubmitField("Enviar")
+class rolForm(FlaskForm):
+    nombre = StringField('nombre')
 
-class ComidaForm(FlaskForm):
-    nombre = StringField('Nombre',validators=[DataRequired()])
-    precio = StringField('precio',validators=[DataRequired()])
-    enviar = SubmitField("Enviar")
+class tecnicoForm(FlaskForm):
+    nombre = StringField('nombre')
+    apellido = StringField('apellido')
+
+class userForm(FlaskForm):
+    email = StringField('email')
+    password = StringField('password')
+    tecnico_id = StringField('tecnico_id')
+    rol_id = StringField('rol_id')
+
+
+class tiposervicioForm(FlaskForm):
+    nombre = StringField('nombre')
+
+class servicioForm():
+    concepto = StringField('concepto')
+    costo = StringField('costo')
+    tecnico_id = StringField('tecnico_id')
+    tipo_servicio_id = StringField('tipo_servicio_id') 
+
+class tipoclienteForm(FlaskForm):
+    nombre = StringField('nombre')
+
+class clienteForm(FlaskForm):
+    nombre = StringField('nombre')
+    telefono = StringField('telefono')
+    email = StringField('email')
+    direccion = StringField('direccion')
+    RFC = StringField('RFC')
+    tipo_clientes_id = StringField('tipo_clientes_id')
+
